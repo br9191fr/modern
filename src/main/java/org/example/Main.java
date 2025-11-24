813893a3be57961b55c303e949f679ac390caae3
Bruno Ricci <brricci@proton.me>
2025-11-24 16:07:01 +0100

    add BC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,14 @@
 package org.example;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import javax.crypto.Cipher;
+import javax.crypto.Cipher.*;
+import java.security.Provider;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import static javax.crypto.Cipher.getMaxAllowedKeyLength;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,7 +18,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println(("Hello and welcome!"));
 
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 10; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
@@ -18,5 +28,25 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("j = " + j);
         }
+        Security.setProperty("crypto.policy", "unlimited");
+        int maxKeySize;
+        try {
+            maxKeySize = Cipher.getMaxAllowedKeyLength("AES");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        //getMaxAllowedKeyLength("AES");
+        System.out.println("Max Key Size for AES : " + maxKeySize);
+        Provider _ = Security.getProvider("BC");
+        Security.addProvider(new BouncyCastleProvider());
+        try {
+            CertificateFactory _ = CertificateFactory
+                    .getInstance("X.509", "BC");
+        } catch (CertificateException | NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println(("Finally OK!"));
+        }
+        System.out.println(("Bye!"));
     }
 }
